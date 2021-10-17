cb90d02a7b2227f2842e475a9242e8bd2348243c
Hitesh Narkhede <hitnarkhede@gmail.com>
2021-10-18 00:40:49 +0530

    Add Function added

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -2,6 +2,7 @@ package calculator;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
@@ -16,5 +17,57 @@ class StringCalculatorShould {
     void string_with_single_number_should_return_number_as_int() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
+    }
+    
+    @Test
+	public void testEmptyString() {
+		assertEquals(0, StringCalculator.add(""));
+	}
+
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, StringCalculator.add("1"));
+	}
+
+	@Test
+	public void testTwoNumbers(){
+		assertEquals(3, StringCalculator.add("1,2"));
+	}
+
+	@Test
+    public void testThreeNumbers(){
+    	assertEquals(6, StringCalculator.add("1,2,3"));
+    }
+
+    @Test
+    public void testNewLine(){
+    	assertEquals(6, StringCalculator.add("1\n2,3"));
+    }
+
+    @Test
+    public void testNegativeNumber(){
+    	try {
+			StringCalculator.add("-1,2");
+		}
+		catch (IllegalArgumentException e){
+			assertEquals(e.getMessage(), "Negatives not allowed: -1");
+		}
+
+		try {
+			StringCalculator.add("2,-4,3,-5");
+		}
+		catch (IllegalArgumentException e){
+			assertEquals(e.getMessage(), "Negatives not allowed: -4,-5");
+		}
+    }
+
+    @Test
+    public void testOverThousand(){
+    	assertEquals(2, StringCalculator.add("1000,2"));
+    }
+
+    @Test
+    public void testOtherDelimiter(){
+    	assertEquals(3, StringCalculator.add("//;\n1;2"));
     }
 }
